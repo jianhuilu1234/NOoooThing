d2d1b20cbcf20e1ff7487d96eabbe8f3745e840a
Lu, jianhui <jianhix,lu@intel.com>
2018-11-07 12:33:10 -0500

    lllll

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+import os
 
+count = 0
 with open("a.txt",'a+') as f:
     f.write("A")
+    data = f.readlines()
+    for line in data:
+        count = count + len(line.strip())
+
+os.system("git commit -am The %s times commit" %count )
+os.system("git push")
